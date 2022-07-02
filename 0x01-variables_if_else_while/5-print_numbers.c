50e6a55e40e333c7f8503c451c14b8a79c3b0cc8
messa <you@example.com>
2022-07-02 12:16:23 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+
+
+
+
+
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+
+
+int main(void)
+
+
+
+{
+
+
+
+	int n;
+
+
+
+
+
+
+
+	for (n = 0; n < 10; n++)
+
+
+
+		printf("%d", n);
+
+
+
+	putchar(10);
+
+
+
+	return (0);
+
+
+
+}
+
+Footer
+
