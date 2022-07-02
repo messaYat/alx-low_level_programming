9cc830a5b8007aa63b5c600e3c78c52adb1fc16a
messa <you@example.com>
2022-07-02 08:08:06 -0700

    size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+
+
+
+
+
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+
+
+int main(void)
+
+
+
+{
+
+
+
+	char a;
+
+
+
+	int b;
+
+
+
+	long int c;
+
+
+
+	long long int d;
+
+
+
+	float e;
+
