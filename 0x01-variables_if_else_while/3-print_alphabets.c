ba8e108f694a47830e3af01ea6a11b12b8f8d60c
messa <you@example.com>
2022-07-02 12:04:32 -0700

    printf

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+
+
+
+
+
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+
+
+int main(void)
+
+
+
+{
+
+
+
+	char n;
+
+
+
+
+
+
+
+	n = 97;
+
+
+
+	while (n < 123)
+
+
+
+		putchar(n++);
+
+
+
+	n = 65;
+
+
+
+	while (n < 91)
+
+
+
+		putchar(n++);
+
+
+
+	putchar(10);
+
+
+
+	return (0);
+
+
+
+}
+
+Footer
+
+© 2022 GitHub, Inc.
+
+Footer navigation
+
+Terms
+
+Privacy
