73270d7bbb33920e0ab1233a4387df362c85fef5
messa <you@example.com>
2022-07-07 16:01:42 -0700

    hot fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -19,12 +19,13 @@ void print_diagonal(int n)
 		for (x = 0; x < n; x++)
 		{
 			for (y = 0; y < x; y++)
-			{	
+				
 				_putchar(32);
-			}
-			_putchar(92);
-			_putchar('\n');
+			 _putchar(92);
+		}
+		_putchar('\n');
 		}
 	}
 }
+
 
