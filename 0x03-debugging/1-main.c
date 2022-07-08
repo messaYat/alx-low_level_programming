26f00feafe24cdc49292dea9ea868ec578791d23
messa <you@example.com>
2022-07-08 14:44:02 -0700

    fix nested function

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,20 +4,19 @@
  * main - causes an infinite loop
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
-
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
 	/**
-	 * while(i < 10)
-	 * {
-	 * 	putchar(i);
-	 *}
-	 */
+ 	* while(i < 10
+ 	* {
+        *  	putchar(i);
+ 
+	*}   
+
+ 	*/ 
 	printf("Infimite lopp avoided! \\o/\n");
 
 
