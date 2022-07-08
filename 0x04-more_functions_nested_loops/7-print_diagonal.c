bd54b8cca3a731e331ce3212c83144639ab2667d
messa <you@example.com>
2022-07-07 17:00:12 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,23 +11,23 @@ void print_diagonal(int n)
 	int x, y;
 
 	if (n <= 0)
-	{	
-	_putchar('\n');
+	{
+		_putchar('\n');
 	}
 	else
 	{
 		for (x = 0; x < n; x++)
 		{
+	
 			for (y = 0; y < x; y++)
 			{
-				_putchar(32);
+			 	_putchar(32);
 			}
-			 _putchar(92);
-		}
+
+		_putchar(92);
 		_putchar('\n');
-		}
 	}
+
 }
 
-
-
+	
