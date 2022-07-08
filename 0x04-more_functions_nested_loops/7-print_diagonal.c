bff929bb84fe17a2075efa01f813c283f20fa1ce
messa <you@example.com>
2022-07-07 19:25:04 -0700

    fixed for second chance

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,28 +12,27 @@ void print_diagonal(int n)
 	
 	if (n <= 0)
 	{
+
 		_putchar('\n');
 	}
 	else
 	{
+
 		for (x = 0; x < n; x++)
 		{
 			for (y = 0; y < x; y++)
 			{
 				_putchar(32);
 			}
-			_putchar(92);
-			_putchar('\n.)
-	
-		}
 
+			_putchar(92);
+			_putchar('\n');
+		}
 	}
 }
 
+			
 
-		
-		
-
-		
+				
 		
 		
