3976298e29837e7b1367ee44c36cd8bf2020d937
messa <you@example.com>
2022-07-12 01:27:43 -0700

    fix this

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,10 +3,8 @@
 /**
 * _strlen -> function to get the length of a string
 * @s: string pointer to passed to this function
-*
 * Return: returns length of the string
 */
-
 int _strlen(char *s)
 {
 	int len;
