52e6b9266bdfcd89d769de1ee1cfe345a5b7c9c5
messa <you@example.com>
2022-07-11 18:08:10 -0700

    4 fixed

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev -> printing a string in reverse
+ * @s: the string printed to be in rev
+ */
+
+void print_rev(char *s)
+{
+	int i, n;
+
+	n = 0;
+	while (s[n] != '\0')
+		n++;
+
+	for (i = n - 1; 1 >= 0;  i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+
