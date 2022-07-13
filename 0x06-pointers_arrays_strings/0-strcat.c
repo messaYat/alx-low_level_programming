1defcb67fce130e2df7567456ac10129702da492
messa <you@example.com>
2022-07-13 13:16:11 -0700

    stract str

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,30 @@
 #include "main.h"
-#include <string.h>
+#include <stdio.h>
 
 /**
-* _stract -> this is a function stract
-* @dest: first param
-* @src: second param
-* Return: a string
-*/
+ * _strcat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string
+ *
+ * Return: A pointer to the resulting string dest
+ */
 char *_strcat(char *dest, char *src)
 {
-	int len = 0, i;
+	int dlen = 0, i;
 
-	while (dest[len])
-		len++;
+	while (dest[dlen])
+	{
+		dlen++;
+	}
 
 	for (i = 0; src[i] != 0; i++)
 	{
-		dest[len] = src[i];
-		len += 1;
+		dest[dlen] = src[i];
+		dlen++;
+
 	}
-	dest[len] = '\0';
+
+	dest[dlen] = '\0';
 	return (dest);
 }
 
