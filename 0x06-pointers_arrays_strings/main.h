2965c16d708fa45999daff2b1bd825716abdaa0d
messa <you@example.com>
2022-07-13 17:08:53 -0700

    strcmp

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *rot13(char *);
