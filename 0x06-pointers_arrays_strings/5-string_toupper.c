37ff203fc97b46732bb3a735047cf46f6f6f81bd
messa <you@example.com>
2022-07-15 06:09:11 -0700

    to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,14 +5,14 @@
 * @x: string param
 * Return: string
 */
-char *string_toupper(char *X)
+char *string_toupper(char *)
 {
 	int i = 0;
 
-	while (x[i])
+	while (X[i])
 	{
-		if (x[i]  >=  97  &&  x[i]  <=  122)
-			x[i]  =  x[i]  -  32;
+		if (X[i]  >=  97  &&  X[i]  <=  122)
+			X[i]  =  X[i]  -  32;
 		i++;
 	}
 	return (X);
