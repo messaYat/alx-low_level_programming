c049be3a5c4f53a2ffe6278fe02c1182f898207b
messa <you@example.com>
2022-07-14 23:29:53 -0700

    hot fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,6 +2,9 @@
 #define MAIN_H
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
+ar *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
 
 
 #endif
