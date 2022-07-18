c8a8b72db71112d837f3f60c893259b9e173ca4f
messa <you@example.com>
2022-07-18 06:55:41 -0700

    hot fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,24 +1,38 @@
 #include "main.h"
 
 /**
-* _strchr -> string character
-* @s: string given
-* @c: another char
-* Return: a string
+* _strchr -> locate character in string
+* @s: source string
+* @c: character to find
+* Return: the string from character found
 */
+
 char *_strchr(char *s, char c)
+
 {
 	int a = 0, b;
 
 	while (s[a]
+	{
+
 		a++;
-	for (b = 0; b < a; b++)
+
+	}
+
+	for (b = 0; b <= a; b++)
+
 	{
 		if c == (s[b])
+
+		{
+
 			s += b;
+
+		}
+
 			return (s);
 	}
+
 	return ('\0');
 }
-
 
