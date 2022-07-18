21313a3bf3c5b464f097c1548620313ce8256bfa
messa <you@example.com>
2022-07-18 05:55:06 -0700

    hot fix memory copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _memcpy -> memory copy
- * @dest: is destination memory
- * @src: is source memory
- * @n: number of bytes to be copied
- * Return: string copied from source
- */
+* _memcpy -> memory copy
+* @dest: is destination memory
+* @src: is source memory
+* @n: number of bytes to be copied
+* Return: string copied from source
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
