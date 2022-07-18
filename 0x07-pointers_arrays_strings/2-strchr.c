6471bcb6062854468e3ec5b6d60dc4d1ef4db304
messa <you@example.com>
2022-07-18 06:28:56 -0700

    alocate strings hot fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strchr -> string character
- * @s: string given
- * @c: another char
- * Return: a string
- */
+* _strchr -> string character
+* @s: string given
+* @c: another char
+* Return: a string
+*/
 char *_strchr(char *s, char c)
 {
 	int a = 0, b;
