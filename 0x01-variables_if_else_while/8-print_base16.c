7662ed0a1f97c596f789ac1780a50290f569dae5
messaYat <mesfinmetta@gmail.com>
2022-12-10 03:25:50 -0800

    print tebephla.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry poinrt 
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	int d;
+	
+	c = 'a';
+	d = 0;
+	while
+		(d < 100) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
