ec48598d1846068699396c572d6a8e41d84d75e2
messaYat <mesfinmetta@gmail.com>
2022-12-10 03:39:27 -0800

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry poinrt 
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
@@ -10,7 +10,7 @@ int main(void)
 	char c;
 
 	int d;
-	
+
 	c = 'a';
 	d = 0;
 	while
