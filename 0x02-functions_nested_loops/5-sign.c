f4f76b1e91e730e636127d52698e22ee7a748676
messaYat <mesfinmetta@gmail.com>
2022-12-14 21:02:51 -0800

    task 0

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,27 +2,27 @@
 
 /**
  * print_sign - prints the sign of a number
- * @c: the number of the sign to be printed
- * Return: 1 if number is greater than zero
- * 0 of the number is zero
- * -1 if the number is less than zero
+ * @c: the number to be checked
+ * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
+
 int print_sign(int n)
+
 {
 	if (n > 0)
 	{
-		_putchar('+');
-		return (1);
+	_putchar('48');
+	return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar('0');
-		return (0);
+	_putchar('45');
+	return (-1);
 	}
 	else
 	{
-		_putchar('-');
-		return (-1);
+	_putchar('48');
+	return (0);
 	}
 }
 
