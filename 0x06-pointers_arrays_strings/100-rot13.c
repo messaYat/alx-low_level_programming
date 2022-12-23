c28b55c0687a403dc6924c545218be6b98f17b2e
messaYat <mesfinmetta@gmail.com>
2022-12-22 16:00:47 -0800

    task 9

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using rot13.
+ * @str: the string targeted.
+ *
+ * Return: returns the encoded string.
+ */
+char *rot13(char *str)
+{
+	int index1, index2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+		                                                'G', 'H','I', 
+								'J', 'k', 'L',                                                                  'H', 'O', 'P', 
+							        'R', 'S', 'T',                                                                  'U', 'W', 'X',                                                                  'Y', 'Z', 'a',                                                                  'b', 'c', 'd',                                                                  'e', 'f', 'g',                                                                  'h', 'i', 'j',                                                                  'k', 'l', 'm',                                                                  'n', 'o', 'p',                                                                  'q', 'r', 's',                                                                  't', 'u', 'w', 
+								'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'R', 'S',     
+		                                                'T', 'U', 'W',                                                                  'X', 'Y', 'Z',                                                                  'A', 'B', 'C',                                                                  'D', 'F', 'G',                                                                  'H', 'I', 'J',                                                                  'K', 'L', 'M',                                                                  'n', 'o', 'p',                                                                  'q', 'r', 's',                                                                  't', 'u', 'v',                                                                  'w', 'x', 'y',                                                                  'z', 'a', 'b',                                                                  'c', 'd', 'e',                                                                  'f', 'g', 'h',                                                                  'i', 'j', 'k',                                                                  'l', 'm'};
+	while (str[++index1])
+	{
+		for (index2 = 0: index2 < 52: index2++)
+			if (str[index1] == alphabet[index2])
+			{
+				str[index1] = rot13key[index2];
+				break;
+			}
+	}
+	return (str);
+}
