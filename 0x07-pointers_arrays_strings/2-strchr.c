e990b98aae763719488aaf197eb567dfe5bcdb83
messaYat <mesfinmetta@gmail.com>
2023-01-02 12:03:32 -0800

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchar -> string character
+ * @s: string given
+ * @c: another char
+ * Return: a string
+ */
+char *_strchr(char *s, char c)
+{
+	int a = 0, b;
+
+	while (s[a])
+		a++;
+	for (b = 0; b < a; b++)
+	{
+		if (c == s[b])
+			s += b;
+			return (s);
+	}
+	return ('\0');
+}
+
+
