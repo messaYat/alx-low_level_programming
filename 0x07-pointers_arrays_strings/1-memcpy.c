37b57a3664a482b807218d44bc6e07eb33b8343e
messaYat <mesfinmetta@gmail.com>
2023-01-03 10:15:45 -0800

    task 1 hot fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,7 +2,7 @@
 /**
  * _memcpy - a function that copies memory area
  * @dest: memory where is stored
- * src: memory where is copied
+ * @src: memory where is copied
  * @n: number of bytes
  *
  * Return: copied memory with n bytes changed
