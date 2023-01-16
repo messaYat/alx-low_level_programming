1c5a76f513fa8aa311c39bce7e9e3807f0504d37
messaYat <mesfinmetta@gmail.com>
2023-01-16 08:53:07 -0800

    task 4 hot fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -54,7 +54,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggo;
 
-	if (name == NULL || age owner == NULL)
+	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
 
 	doggo = malloc(sizeof(dog_t));
