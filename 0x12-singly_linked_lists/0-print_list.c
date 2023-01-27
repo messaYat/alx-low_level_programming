41ebd365863d9bf6b724b9c2a5508a8977f64842
messa <mesfinmetta@gmail.com>
2023-01-27 06:57:51 -0800

    task 0 fixed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,17 +14,14 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
-			printf("[0] (nll)\n");
+			printf("[0] (nil)\n");
 
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
 		nodes++;
 		h = h->next;
-
 	}
 
 	return (nodes);
 }
-
-
