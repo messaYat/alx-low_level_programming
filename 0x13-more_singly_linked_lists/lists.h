ba5014f3de61405d322cfa53c76a5df85ae6e113
messa <mesfinmetta@gmail.com>
2023-01-31 20:18:26 -0800

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,10 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdio.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -13,11 +12,11 @@
  * Description: singly linked list node structure
  *
  */
-typedef struct listint_s
+typedef struct listint_s;
 {
 	int n;
 	struct listint_s *next;
-} listint_t;
+} listint_s;
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
