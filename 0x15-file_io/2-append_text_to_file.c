1c5a96802ff1e10c35dd81aca4f50e367f018405
messa <mesfinmetta@gmail.com>
2023-02-06 12:18:44 -0800

    task 2 edited

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,5 @@
+#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 
-		if (write(fd, text_content, _strlen(text_content)) == -1)
+		if (write(fd, text_content, strlen(text_content)) == -1)
 			return (-1);
 	}
 
