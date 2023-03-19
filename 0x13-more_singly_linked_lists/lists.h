b4dfd667bc7831fdf14355d117cc639766a58757
messa <mesfinmetta@gmail.com>
2023-03-19 06:38:30 -0700

    print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,22 +1,21 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _S_LINK_LISTS_
+#define _S_LINK_LISTS_
 
-#include <stdio.h>
-#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for alx project
+ * for Holberton project
  */
-typedef struct listint_s;
+typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
-size_t print_listint(const listint_t, *h);
+
+size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
@@ -27,5 +26,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
 
 #endif
