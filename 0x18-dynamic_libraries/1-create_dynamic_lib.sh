c47870dab43c45686408e3752fc533143adb404e
messa <mesfinmetta@gmail.com>
2023-03-24 11:44:06 -0700

    DL task 1 edited

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc --Wall -Wextra -pedantic -c -fPIC *.c
-gcc -shared -o liball.so *o
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
+gcc -shared -o liball.so *.o
 export LD_LIRARY_PATH=.:$LD_LIBRARY_PATH
