8555eb2c67d6718f992d0e2c653024ad6f9ed97b
messa <mesfinmetta@gmail.com>
2023-05-21 05:46:36 -0700

    main

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,17 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-<<<<<<< HEAD
-=======
 int get_endianness(void);
->>>>>>> 26dabf09cdd65f875b4ea4f92f7f15fa03f0ecb1
 
-#endif /* MAIN_H */
-
+#endif
