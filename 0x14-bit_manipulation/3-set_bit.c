36beecfad892ff08e109e3e9aef5d72ff1c0cd6a
messa <mesfinmetta@gmail.com>
2023-05-21 05:35:54 -0700

    bit mainpulation 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,18 +1,15 @@
 #include "holberton.h"
-
 /**
- * set_bit - sets the value of a bit to 1 at a given index.
- * @n: number.
- * @index: index.
- * Return: 1 if it worked, or -1 if an error occurred.
+ * set_bit - Entry Point
+ * @n: input
+ * @index: index
+ * Return: 0
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int aux = 1 << index;
-
-	if (index > (sizeof((*n)) * 8))
+	if (index > 63)
 		return (-1);
 
-	*n = *n | aux;
+	*n = *n | 1ul << index;
 	return (1);
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,19 +1,18 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * clear_bit - sets the value of a bit at a given index to 0.
- * @n: A pointer to the bit.
- * @index: The index to set the value at - indices start at 0.
- *
- * Return: If an error occurs - -1.
- *         otherwise - 1.
+ * clear_bit - sets the value of a bit to 0 at a given index.
+ * @n: number.
+ * @index: index, starting from 0 of the bit you want to set.
+ * Return: 1 if it worked, or -1 if an error occurred.
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 0))
+	unsigned long int aux =  ~(1 << index);
+
+	if (index > (sizeof((*n)) * 8))
 		return (-1);
 
-	*n &= -(1 << index);
-	
+	*n = *n & aux;
 	return (1);
 }
